0ae8df755fe5b4efc693e36869946ebde9fa4fc4
jill <oatmealine@disroot.org>
2021-07-22 02:59:52 +0300

    added all the xml stuff

---
+++ Scripts/RichPresence.lua
@@ -0,0 +1,58 @@
+-- simple script that lets you put stuff into preferences in correspondance in whats happening In Game
+-- the actual implementation is in xmls assorted across the theme :/
+
+oat_RPC = {}
+oat_RPC.data = {}
+
+local identifier = 'OATRPC@'
+local seperator = '@'
+local seperator2 = ':'
+
+local dataValues = {
+  state = 'string', -- 'Menu', 'Gameplay', 'Idle', 'Results'
+
+  -- Gameplay / Results
+  title = 'string',
+  author = 'string',
+  pack = 'string',
+  
+  -- Gameplay
+  songstart = 'number', -- timestamp
+  songend = 'number', -- timestamp
+
+  -- Results
+  score = 'number',
+
+  -- Menu
+  --browsingpack = 'string',
+  browsingsince = 'number', -- timestamp
+
+  -- Idle
+  --idlesince = 'number', -- timestamp
+}
+
+function oat_RPC:set(key, val, force)
+  if dataValues[key] == type(val) then
+    oat_RPC.data[key] = val
+    return val
+  elseif not key then
+    error('no key given', 2)
+  elseif not dataValues[key] then
+    error(key .. ' is not a valid key', 2)
+  else
+    error('invalid type for ' .. key .. ', should be ' .. dataValues[key] .. ', got ' .. type(val), 2)
+  end
+end
+function oat_RPC:get(key)
+  return oat_RPC.data[key]
+end
+function oat_RPC:update()
+  local s = identifier
+
+  for k,v in pairs(oat_RPC.data) do
+    s = s .. k .. seperator2 .. v .. seperator
+  end
+
+  --print(s)
+  PREFSMAN:SetPreference('LastSeenVideoDriver', s)
+end
