5d337ddaacf0503ef2717779ed125fdfe2a24cc8
jill <oatmealine@disroot.org>
2021-07-21 17:09:49 +0300

    unhardcoded random texts

--- Scripts/Theme.lua
+++ Scripts/Theme.lua
@@ -42,3 +42,116 @@ function wrap(val,max,min)
 	return math.mod(val+(max-min+1)-min,max-min+1)+min
 end
 
+
+
+-- randomized headers, titles, static strings, etc, get it all here
+
+ScreenTitleMenuHeaderTexts = {
+  'go on. select something',
+}
+function ScreenTitleMenuHeaderTextSet(self) self:settext(ScreenTitleMenuHeaderTexts[math.random(1, #ScreenTitleMenuHeaderTexts)]) end
+
+ScreenSelectGameHeaderTexts = {
+  'Game mode Sele c t tionnjfdhmngfgnfdmfythgnjmgfhbdxkjgsevrlbnkhj',
+}
+function ScreenSelectGameHeaderTextSet(self) self:settext(ScreenSelectGameHeaderTexts[math.random(1, #ScreenSelectGameHeaderTexts)]) end
+
+ScreenSelectColorHeaderTexts = {
+  'color time babey!!!',
+}
+function ScreenSelectColorHeaderTextSet(self) self:settext(ScreenSelectColorHeaderTexts[math.random(1, #ScreenSelectColorHeaderTexts)]) end
+
+ScreenSelectMusicHeaderTexts = {
+  'hey hey ..... paly the big black',
+  'oatmealine is the only folder that matters',
+  'go on. select chance',
+  'Are You In The Groove?',
+  'you have 3 seconds to get in the groove',
+  'penis music.',
+  'Barack Obama',
+  'insert funy here',
+  'Cock',
+  'oatmealine',
+  'Open the !wips. Now',
+  ':pensive:',
+  'huge nintendo ds',
+  'the burger',
+  'h',
+  '             e',
+  '',
+  'HOW',
+  'play forbidden. now',
+  ':sunglasses:',
+  'play the ' .. math.random(1, 200) .. 'th pack in your collection',
+  'WEED',
+}
+function ScreenSelectMusicHeaderTextSet(self) self:settext(ScreenSelectMusicHeaderTexts[math.random(1, #ScreenSelectMusicHeaderTexts)]) end
+
+ScreenPlayerOptionsHeaderTexts = {
+  'get some mods, its free',
+}
+function ScreenPlayerOptionsHeaderTextSet(self) self:settext(ScreenPlayerOptionsHeaderTexts[math.random(1, #ScreenPlayerOptionsHeaderTexts)]) end
+
+ScreenEvaluationStageHeaderTexts = {
+  'you suck',
+  '#score-share time',
+  'press PRTSCR to kill a family',
+  'sonic the hedghehog',
+  'penis!!!',
+  'HOW',
+  'YOU ROCK!',
+  'm',
+  'h',
+  ',',
+  'Cocke and Balls',
+  'haha your sync is broken', 'gaming!!',
+  'good job, She is proud of you',
+  'hacking hacker.....',
+  'wooow... not even an FC?? you suck',
+  'PENIS',
+  '',
+  'wii phone',
+  'xbox 720',
+  'cant believe',
+  'pain',
+  'man',
+  'horse',
+  'cant believe they made taiko in nitg.....',
+  'COCK',
+  'pussy',
+  'go post this on twitter. go brag about your score',
+  'wouldnt it be a shame if... AV',
+  'good job! the game will now AV',
+  'music do be banging doe',
+  'soulja boy tell em',
+  'Polish toilet spin',
+  'SCREENMAN:GetTopScreen():vibrate()',
+  'h.how',
+  'HOW IS THIS WOMAN',
+  'ronaldinho soccer!!!',
+  'wow youre terrible',
+  'keyboard moment',
+  'glitch2.frag moment',
+  'it wasnt fault my game lagged',
+  'skewllex',
+  'what the Fuck',
+  'benis',
+  'weewee',
+  'wenis!!!!!',
+  'oatmealine is gay',
+  'tung tiied gaming',
+  'kfc gaming',
+  'Denis',
+  'bussy',
+  '666 is a 14',
+  'bye-bye',
+  'skye is cute',
+  'wahoo',
+  'caulk',
+}
+function ScreenEvaluationStageHeaderTextSet(self) self:settext(ScreenEvaluationStageHeaderTexts[math.random(1, #ScreenEvaluationStageHeaderTexts)]) end
+
+ScreenSelectMusicCourseHeaderTexts = {
+  'select cour se',
+}
+function ScreenSelectMusicCourseHeaderTextSet(self) self:settext(ScreenSelectMusicCourseHeaderTexts[math.random(1, #ScreenSelectMusicCourseHeaderTexts)]) end
