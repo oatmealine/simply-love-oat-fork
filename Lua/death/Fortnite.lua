279000e618962dabc3cc070b89ecb6bfc7261a42
Jecket22 <hark.nothing@gmail.com>
2021-10-30 13:12:31 +0200

    WTF i made it actually work

--- Lua/death/Fortnite.lua
+++ Lua/death/Fortnite.lua
@@ -2,13 +2,14 @@ local fortnite = {}
 
 function fortnite:Setup()
     self(1):cmd("stretchto,0,0,SCREEN_WIDTH,SCREEN_HEIGHT;diffusealpha,0") -- I hope you play only at 4:3
-	math.randomseed(math.random() * 1000000 + Second() * Hour() * DayOfYear())
-	self(1):Load(Path('Graphics', '../Screens/Overlay/Death/Fortnite/' .. math.random(1, 22)))
 end
 
 function fortnite:Dead()
+	math.randomseed(math.random() * 1000000 + Second() * Hour() * DayOfYear()) -- this is jank
+	self(1):Load(Path('Graphics', '_fortniteDeath/_' .. math.random(1, 22)))
+	self(1):cmd("stretchto,0,0,SCREEN_WIDTH,SCREEN_HEIGHT;diffusealpha,0")
     GAMESTATE:ApplyGameCommand('sound,vine_thud')
-	self(1):cmd("sleep,.16666;diffusealpha,1;sleep,.277;diffusealpha,0;")
+	self(1):cmd("sleep,.2;diffusealpha,1;sleep,.277;diffusealpha,0;")
 end
 
 return fortnite
