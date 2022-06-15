cdf517c8886ff75f740b79d9698d85d0dd636739
NyaightHazard <13866033+NyaightHazard@users.noreply.github.com>
2022-06-15 21:35:51 +0900

    Add "Half Bar" option and change bar BG color

--- Scripts/OatOptions.lua
+++ Scripts/OatOptions.lua
@@ -14,7 +14,7 @@ OatProfile().OATDisplayCustomHeaders = OatProfile().OATDisplayCustomHeaders or 1
 OatProfile().OATDisplayFriendZ = OatProfile().OATDisplayFriendZ or 1
 OatProfile().OATResultGif = OatProfile().OATResultGif or 2
 OatProfile().OATResultText = OatProfile().OATResultText or 2
-OatProfile().OATResultRate = OatProfile().OATResultRate == nil and true or OatProfile().OATResultRate
+OatProfile().OATResultRate = OatProfile().OATResultRate or 1
 OatProfile().OATBackgroundShader = OatProfile().OATBackgroundShader or 2
 OatProfile().OATRichPresence = OatProfile().OATRichPresence == nil and true or OatProfile().OATRichPresence
 OatProfile().OATResultsAlpha = OatProfile().OATResultsAlpha or 2 -- floating point
@@ -122,11 +122,12 @@ end
 function OptionDisplayResultJudgeRate()
 	local t = OptionRowBase('ResultJudgeRate')
 	  t.OneChoiceForAllPlayers = true
-	  t.Choices = {'On', 'Off'}
-	t.LoadSelections = function(self, list) if OatProfile().OATResultRate then list[1] = true else list[2] = true end end
+	  t.Choices = {'Full Bar', 'Half Bar', 'Off'}
+	t.LoadSelections = function(self, list) if OatProfile().OATResultRate then list[OatProfile().OATResultRate] = true else list[1] = true end end
 	  t.SaveSelections = function(self, list)
-		  if list[1] then OatProfile().OATResultRate = true end
-		  if list[2] then OatProfile().OATResultRate = false end
+		  if list[1] then OatProfile().OATResultRate = 1 end
+		  if list[2] then OatProfile().OATResultRate = 2 end
+		  if list[3] then OatProfile().OATResultRate = 3 end
 	  resetHeader()
 	  resetBackground()
 	  end
